c6195fbe007cf0bf2c4c529ccf340417bf9bcdfc
bestmaddy <mandeepwadekar@gmail.com>
2025-06-30 09:11:26 +0530

    env file change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: true,
-  url: process.env["NG_APP_BASE_URL"]
-
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  url: process.env["NG_APP_BASE_URL"]
 };
 
 /*
